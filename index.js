fff984f489a6b5b7c2f6ff0f523187c04a729b23
zawadinjagi <zawadinjagi619@gmail.com>
2024-03-13 16:15:49 +0300

    add

---
+++ index.js
@@ -0,0 +1,22 @@
+function introduction(name) {
+    console.log(`Hi, my name is Aki.`);
+    return `Hi, my name is ${name}.`;
+  }
+
+function introduction(name) {
+    console.log(`Hi, my name is Samip.`);
+    return `Hi, my name is ${name}.`;
+  } 
+
+function introductionWithLanguage(name = "Aki", language ="Ember.js") {
+    if (typeof string === "name, language");
+
+    console.log(
+        `Hi, my name is ${name} and I am learning to program in ${language}.`
+    );
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+  }
+
+function introductionWithLanguageOptional(name = "Gracie", language = "JavaScript") {
+   return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+  }
